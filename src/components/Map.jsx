e61626822d3eb0d07ea9da14a5e1eb1db45cb252
Akamtemitope <temitopeakam@gmail.com>
2023-07-23 01:13:10 +0300

    add markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,41 @@
 import L from "leaflet";
 import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Polyline,
+  Marker,
+  CircleMarker,
+  Popup,
+  LayerGroup,
+} from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import { randomColor } from "../helpers";
+import startIcon from "/marker-start.png";
+import endIcon from "/marker-end.png";
+
+const customIcon = (url) =>
+  L.icon({
+    iconUrl: url,
+    iconSize: [32, 32],
+  });
 
 const Map = () => {
   const mapRef = useRef();
   const selectedRouteId = useSelector((state) => state.routes.selectedRouteId);
   const routesPolyline = useSelector((state) => state.routes.routesPolyline);
 
+  const mapMarkers = (markers) =>
+    markers.map((latLng, i) => (
+      <CircleMarker
+        key={latLng[0] + i}
+        center={latLng}
+        radius={5}
+        fillColor={randomColor()}
+      />
+    ));
   useEffect(() => {
     if (selectedRouteId && routesPolyline[selectedRouteId]) {
       const bounds = L.latLngBounds(routesPolyline[selectedRouteId]);
@@ -30,12 +55,30 @@ const Map = () => {
       />
       {Object.keys(routesPolyline).map((key, index) => {
         return (
-          <Polyline
-            key={key + index}
-            positions={routesPolyline[key]}
-            weight={7}
-            color={randomColor()}
-          />
+          <LayerGroup key={key + index}>
+            <Polyline
+              positions={routesPolyline[key]}
+              weight={7}
+              color={randomColor()}
+            />
+            {selectedRouteId === key ? (
+              <>
+                <Marker
+                  position={routesPolyline[key][0]}
+                  icon={customIcon(startIcon)}
+                >
+                  <Popup>Start Destination</Popup>
+                </Marker>
+                <Marker
+                  position={routesPolyline[key][routesPolyline[key].length - 1]}
+                  icon={customIcon(endIcon)}
+                >
+                  <Popup>End Destination</Popup>
+                </Marker>
+                {mapMarkers(routesPolyline[key])}
+              </>
+            ) : null}
+          </LayerGroup>
         );
       })}
     </MapContainer>
