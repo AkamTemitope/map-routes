41713cb56768ccb554c03c4127e8de78b9ebd2e5
Akamtemitope <temitopeakam@gmail.com>
2023-07-23 13:23:33 +0300

    change profile

--- src/services/osrmService.js
+++ src/services/osrmService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchRoutePolyline = (coordinates) => {
-  const osrmUrl = `http://router.project-osrm.org/route/v1/driving/${coordinates}?overview=full&geometries=geojson&continue_straight=false&steps=true&annotations=false`;
+  const osrmUrl = `http://router.project-osrm.org/route/v1/foot/${coordinates}?overview=full&geometries=geojson&continue_straight=false&steps=true&annotations=false`;
   return axios.get(osrmUrl);
 };
 
